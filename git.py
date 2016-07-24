a81866b2e17a8e4531e517d6201c05dbca7d38e9
David Goffredo <dmgoffredo@gmail.com>
2016-07-23 20:42:52 -0400

    git module is working

--- git.py
+++ git.py
@@ -7,24 +7,39 @@ import os
 def _dirOf(filePath):
     return os.path.dirname(os.path.realpath(filePath))
 
+# Returns (returnCode, stdout)
+# where stdout is stripped of trailing whitespace.
+#
+def _callInDirOf(filePath, command):
+    with open(os.devnull, 'wb') as devnull:
+        process = subprocess.Popen(command,
+                                   cwd=_dirOf(filePath),
+                                   stdout=subprocess.PIPE,
+                                   stderr=devnull)
+        output, _ = process.communicate()
+        return process.returncode, output.rstrip()
+
 def inAnyRepo(filePath):
-    return 0 == subprocess.call(['git', 'rev-parse'],
-                                cwd=_dirOf(filePath),
-                                stdout=os.devnull, 
-                                stderr=devnull)
+    rc, _ = _callInDirOf(filePath, ['git', 'rev-parse'])
+    return rc == 0
 
 def getHeadRevision(filePath):
-    pass # TODO
+    rc, output = _callInDirOf(filePath, ['git', 'rev-parse', 'HEAD'])
+    assert rc == 0, "Can't get revision. Is {} in a git repo?".format(filePath)
+    return output
 
 def diffHead(filePath):
-    pass # TODO
+    rc, output = _callInDirOf(filePath, 
+                              ['git', 'diff', os.path.basename(filePath)])
+    assert rc == 0, "Can't diff file. Is {} in a git repo?".format(filePath)
+    return output
 
 if __name__ == '__main__':
     import sys
     if len(sys.argv) < 2:
         sys.exit() # Nothing to do
 
-    filePath = argv[1]
+    filePath = sys.argv[1]
     isInGit = inAnyRepo(filePath)
     print('In git repository? ', 'True' if isInGit else 'False')
     if isInGit:
