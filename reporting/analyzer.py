b337b1d62aa3dc3bd95ea9ff769cfafbd670a11c
David Goffredo <dmgoffredo@gmail.com>
2016-07-28 01:29:29 -0400

    Stub for post-parse analysis

---
+++ reporting/analyzer.py
@@ -0,0 +1,10 @@
+
+# TODO
+#
+# This is the module that will take the parsed plot description file and
+# determine whether it's well-formed, collect all SQL queries and plots
+# defined in the file, and produce a data structure that can be used bytearray
+# the report generator.
+
+def analyze(parsedDefinitions):
+    pass # TODO
