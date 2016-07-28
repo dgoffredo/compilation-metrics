695e085d7acffcfb5a4dda46f7c6832db8780f08
David Goffredo <dmgoffredo@gmail.com>
2016-07-28 01:40:39 -0400

    stupid auto-complete

--- reporting/analyzer.py
+++ reporting/analyzer.py
@@ -3,7 +3,7 @@
 #
 # This is the module that will take the parsed plot description file and
 # determine whether it's well-formed, collect all SQL queries and plots
-# defined in the file, and produce a data structure that can be used bytearray
+# defined in the file, and produce a data structure that can be used by the
 # the report generator.
 
 def analyze(parsedDefinitions):
