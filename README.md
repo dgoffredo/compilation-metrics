235eaf9a7b0c8580406e065ec78704a91bb109d4
David Goffredo <dmgoffredo@gmail.com>
2016-08-05 01:39:16 -0400

    README

--- README.md
+++ README.md
@@ -1,1 +1,26 @@
-TODO
+# Compilation Metrics
+Keep track of what's taking so long to compile.
+
+Compilation Metrics has two components: 
+* a wrapper script for your compiler that collects statistics on the time and
+  resources consumed compiling each file, outputting the collected statistics
+  to a database.
+* a report generation tool that, provided a markdown file and a description of
+  diagrams to produce, renders a website from the statistics database.
+
+## Dependencies
+Compilation Metrics is written in python. One goal of the project is to have
+as few dependencies as possible, so that it can be used as a drop-in 
+replacement for the compiler in most build environments.
+
+Collecting statistics requires the following dependencies:
+* unix/linux
+* python 2.7+
+* git
+
+Report generation has the following additional dependencies:
+* gnuplot
+* perl
+
+## Usage
+TBD
---
+++ reporting/scratch.py
@@ -0,0 +1,42 @@
+
+# New modules:
+#    database: Runs the queries
+#    plotter:  Renders the plots
+#    appendix: Generates the detail html page (not the markdown report)
+
+# New classes and generators:
+#    appendix.Builder: streams to HTML
+#    analyzePlotsFile: spits out named plot descriptors
+#    plotter.Renderer: streams points to a gnuplot process that writes png,
+#                      and will rotate the image afterwards if necessary.
+#    database.query:   connects to database, creates view(s), and spits out
+#                      query results as they come in.        
+
+'''
+
+Lines are read from input and passed through the data pipeline shown below.
+
+The stream of lines is passed through a lexer and parser and then analyzed 
+to yield a stream of plot descriptors, which are then passed along with a
+database path into a query engine whose result records are then streamed
+into an output file (per plot), a gnuplot-generated image (per-plot), and
+an "appendix" HTML file.
+
+                                                             +--> [data file]
+                                                             |
+[input] --> [lex] --> [parse] --> [analyze] --+              +--> [gnuplot]
+                                              |              |
+                                              +--> [query] --+
+                                              |              |
+[database] ----------------->-----------------+              +--> [appendix]
+
+'''
+
+with appendix.Builder(rootFolder, plotFolder, dataFolder) as appendixBuilder:
+    for plot in analyzePlotsFile(...):
+        appendixBuilder.beginPlot(plot)
+        with plotter.Renderer(plot, imageFolder) as renderer:
+            for dataRecord in database.query(plot):
+                queryDataFile.addRecord(dataRecord)
+                renderer.addRecord(dataRecord)
+                appendixBuilder.addRecord(dataRecord)
