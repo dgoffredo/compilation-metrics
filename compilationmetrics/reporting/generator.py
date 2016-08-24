f7039c7edac7fc3ec19ded044869f35a6370c65b
David Goffredo <dmgoffredo@gmail.com>
2016-08-23 22:55:22 -0400

    Copyright notice.

--- compilationmetrics/reporting/generator.py
+++ compilationmetrics/reporting/generator.py
@@ -1,6 +1,5 @@
 
 '''
-
 Lines are read from input and passed through the data pipeline shown below.
 
 The stream of lines is passed through a lexer and parser and then analyzed 
@@ -16,7 +15,6 @@ an "appendix" HTML file.
                                               +--> [query] --+
                                               |              |
 [database] ----------------->-----------------+              +--> [appendix]
-
 '''
 
 import gnuplot
@@ -71,3 +69,25 @@ if __name__ == '__main__':
     rootFolder = '.'
     with open(sys.argv[1]) as reportMarkdownFile:
         generate(plotsFile, reportMarkdownFile, rootFolder)
+
+'''
+Copyright (c) 2016 David Goffredo
+
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in
+all copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+SOFTWARE.
+'''
